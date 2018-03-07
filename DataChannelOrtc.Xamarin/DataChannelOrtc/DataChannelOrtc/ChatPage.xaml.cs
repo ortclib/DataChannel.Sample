74deb3c3d645af4ddcfa0a8b3664ac98493f2579
ivanab <ivana@opticaltone.com>
2018-03-07 19:14:55 +0100

    Added Xamarin.Forms iOS sample

---
+++ DataChannelOrtc.Xamarin/DataChannelOrtc/DataChannelOrtc/ChatPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using DataChannelOrtc.Signaling;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataChannelOrtc
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatPage : ContentPage
+    {
+        public ObservableCollection<Message> _messages = new ObservableCollection<Message>();
+
+        public event EventHandler RemotePeerConnected;
+        public event EventHandler RemotePeerDisconnected;
+        public event EventHandler<Message> SendMessageToRemotePeer;
+
+        private event EventHandler<Message> MessageFromRemotePeer;
+
+        public void HandleRemotePeerConnected()
+        {
+            RemotePeerConnected?.Invoke(this, null);
+        }
+
+        public void HandleRemotePeerDisonnected()
+        {
+            RemotePeerDisconnected?.Invoke(this, null);
+        }
+
+        public void HandleMessageFromPeer(string message)
+        {
+            MessageFromRemotePeer?.Invoke(this, new Message(RemotePeer, LocalPeer, DateTime.Now, message));
+        }
+
+        private bool _isSendReady = false;
+        public bool IsSendReady
+        {
+            get { return _isSendReady; }
+            set { _isSendReady = value; }
+        }
+
+        public Peer LocalPeer { get; set; }
+        public Peer RemotePeer { get; set; }
+
+        public ChatPage(Peer localPeer, Peer remotePeer)
+        {
+            LocalPeer = localPeer;
+            RemotePeer = remotePeer;
+
+            InitializeComponent();
+
+            this.RemotePeerConnected += Signaler_RemoteConnected;
+            this.RemotePeerDisconnected += Signaler_RemoteDisconnected;
+            this.MessageFromRemotePeer += Signaler_MessageFromRemotePeer;
+
+            InitView();
+        }
+
+        private void Signaler_RemoteConnected(object sender, EventArgs e)
+        {
+            IsSendReady = true;
+            btnSend.IsEnabled = true;
+            btnSend.BackgroundColor = Color.Gray;
+        }
+
+        private void Signaler_RemoteDisconnected(object sender, EventArgs e)
+        {
+            IsSendReady = false;
+            btnSend.IsEnabled = false;
+            btnSend.BackgroundColor = Color.DarkGray;
+        }
+
+        private void Signaler_MessageFromRemotePeer(object sender, Message message)
+        {
+            _messages.Add(new Message(DateTime.Now, RemotePeer, message.Text));
+
+            ScrollMessages();
+        }
+
+        private void InitView()
+        {
+            listMessages.ItemsSource = _messages;
+            entryMessage.Text = string.Empty;
+            btnSend.IsEnabled = false;
+            btnSend.BackgroundColor = Color.DarkGray;
+
+            // Page structure 
+            Content = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    listMessages,
+                    slMessage
+                }
+            };
+
+            btnSend.Clicked += (sender, args) =>
+            {
+                if (entryMessage.Text != string.Empty)
+                {
+                    var message = new Message(LocalPeer, RemotePeer, DateTime.Now, entryMessage.Text);
+                    OnSendMessageToRemotePeer(message);
+
+                    _messages.Add(new Message(DateTime.Now, LocalPeer, entryMessage.Text));
+
+                    ScrollMessages();
+                }
+                entryMessage.Text = string.Empty;
+            };
+        }
+
+        private void OnSendMessageToRemotePeer(Message message)
+        {
+            SendMessageToRemotePeer?.Invoke(this, message);
+        }
+
+        private void ScrollMessages()
+        {
+            if (_messages.Count > 0)
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                    listMessages.ScrollTo(_messages[_messages.Count - 1],
+                    ScrollToPosition.End, true));
+            }
+        }
+    }
+}
---
+++ DataChannelOrtc.Xamarin/DataChannelOrtc/DataChannelOrtc/Message.cs
@@ -0,0 +1,35 @@
+﻿using DataChannelOrtc.Signaling;
+using System;
+
+namespace DataChannelOrtc
+{
+    public class Message
+    {
+        public Peer Author { get; set; }
+        public Peer Recipient { get; set; }
+        public Peer SendingPeer { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Time { get; set; }
+        public string Text { get; set; }
+        public string AuthorName { get; set; }
+
+        public Message() { }
+
+        public Message(DateTime date, Peer author, string text)
+        {
+            Time = date.ToString("h:mm");
+            AuthorName = author.Name;
+            Text = text;
+        }
+
+        public Message(Peer author, Peer recipient, DateTime date, string text)
+        {
+            Author = author;
+            Recipient = recipient;
+            DateTime = date;
+            Text = text;
+        }
+    }
+}
